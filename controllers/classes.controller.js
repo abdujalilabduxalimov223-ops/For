3fe67a99bd58b1b576fa20e0a87511792d11f4d7
abdujalilabduxalimov223-ops <abdujalilabduxalimov223@gmail.com>
2026-03-06 12:36:47 +0500

    Update classes.controller.js

--- controllers/classes.controller.js
+++ controllers/classes.controller.js
@@ -3,29 +3,67 @@ const path = require("path");
 
 const filePath = path.join(__dirname, "../data/classes.json");
 
-function readData() {
-  return JSON.parse(fs.readFileSync(filePath, "utf-8"));
+
+function readData(){
+
+ if(!fs.existsSync(filePath)){
+  fs.writeFileSync(filePath, JSON.stringify({},null,2));
+ }
+
+ return JSON.parse(fs.readFileSync(filePath,"utf-8"));
+
 }
 
-function writeData(data) {
-  fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
+
+function writeData(data){
+ fs.writeFileSync(filePath, JSON.stringify(data,null,2));
 }
 
-exports.getClassData = (req, res) => {
-  const { classType } = req.params;
+
+
+exports.getClassData = (req,res)=>{
+
+ try{
+
+  const {classType} = req.params;
+
   const data = readData();
 
   res.json(data[classType] || {});
+
+ }catch(err){
+
+  res.status(500).json({error:"Server xatosi"});
+
+ }
+
 };
 
-exports.addClassData = (req, res) => {
-  const { classType } = req.params;
+
+
+exports.addClassData = (req,res)=>{
+
+ try{
+
+  const {classType} = req.params;
+
   const newData = req.body;
 
   const data = readData();
-  data[classType] = newData;
+
+  data[classType] = {
+   ...data[classType],
+   ...newData
+  };
 
   writeData(data);
 
-  res.json({ success: true });
+  res.json({success:true});
+
+ }catch(err){
+
+  res.status(500).json({error:"Saqlashda xato"});
+
+ }
+
 };
