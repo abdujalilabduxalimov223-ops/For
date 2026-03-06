30f92444b6aa1c6ebab157cac349520d8f0c6475
abdujalilabduxalimov223-ops <abdujalilabduxalimov223@gmail.com>
2026-03-06 12:38:22 +0500

    Update resources.controller.js

--- controllers/resources.controller.js
+++ controllers/resources.controller.js
@@ -3,20 +3,44 @@ const path = require("path");
 
 const dataPath = path.join(__dirname, "../data/resources.json");
 
+
 function readData() {
+
+  if (!fs.existsSync(dataPath)) {
+    fs.writeFileSync(dataPath, JSON.stringify([], null, 2));
+  }
+
   return JSON.parse(fs.readFileSync(dataPath, "utf-8"));
+
 }
+
 
 function writeData(data) {
   fs.writeFileSync(dataPath, JSON.stringify(data, null, 2));
 }
 
+
+// GET
 exports.getResources = (req, res) => {
-  res.json(readData());
+
+  const data = readData();
+  res.json(data);
+
 };
 
+
+// ADD
 exports.addResource = (req, res) => {
-  const resources = readData();
+
+  if (!req.file) {
+    return res.status(400).json({ error: "Fayl yuklanmadi" });
+  }
+
+  let resources = readData();
+
+  if (!Array.isArray(resources)) {
+    resources = [];
+  }
 
   resources.push({
     id: Date.now(),
@@ -25,29 +49,44 @@ exports.addResource = (req, res) => {
     filename: req.file.filename,
     folder: req.body.category,
     size: (req.file.size / 1024 / 1024).toFixed(2) + " MB",
-    type: path.extname(req.file.originalname).substring(1).toUpperCase()
+    type: path.extname(req.file.originalname)
+      .substring(1)
+      .toUpperCase()
   });
 
   writeData(resources);
+
   res.json({ success: true });
+
 };
 
+
+// DELETE
 exports.deleteResource = (req, res) => {
+
   let resources = readData();
+
   const file = resources.find(r => r.id == req.params.id);
 
   if (file) {
+
     const filePath = path.join(
       __dirname,
       "../uploads",
       file.folder,
       file.filename
     );
-    if (fs.existsSync(filePath)) fs.unlinkSync(filePath);
+
+    if (fs.existsSync(filePath)) {
+      fs.unlinkSync(filePath);
+    }
+
   }
 
   resources = resources.filter(r => r.id != req.params.id);
+
   writeData(resources);
 
   res.json({ success: true });
+
 };
